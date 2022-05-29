8d9ff6d8601dbf57618f434b202ad2e0aabce0b4
Huseyin Aykis <h.aykis25@gmail.com>
2022-05-29 15:47:53 +0200

    yeni klas

---
+++ src/test/java/com/infoTech/Tests/deneme55.java
@@ -0,0 +1,12 @@
+package com.infoTech.Tests;
+
+import com.infoTech.utilities.TestBase;
+import org.testng.annotations.Test;
+
+public class deneme55 extends TestBase {
+    @Test
+    public void orneksayfa(){
+        driver.get("https:google.com");
+    }
+
+}
